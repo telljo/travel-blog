a71432be9fd7346809621143309f7d2d517003ba
Josh Tell <jtell1997@gmail.com>
2024-07-05 08:32:50 +1200

    Tidy up CRUD views for trips and posts. Also fix dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,24 +3,32 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dropdown"
 export default class extends Controller {
 
-  static targets = ["menu", "buttonText", "checkbox"]
+  static targets = ["menu", "buttonText", "checkbox", "icon"]
 
   toggle() {
-    if (this.menuTarget.classList.contains('toggled')) {
-      this.menuTarget.classList.remove('toggled');
+    if (this.menuTarget.classList.contains('dropdown__menu--toggled')) {
+      this.menuTarget.classList.remove('dropdown__menu--toggled');
     } else {
-      this.menuTarget.classList.add('toggled');
+      this.menuTarget.classList.add('dropdown__menu--toggled');
+    }
+
+    if (this.iconTarget.classList.contains('icon--toggled')) {
+      this.iconTarget.classList.remove('icon--toggled');
+    } else {
+      this.iconTarget.classList.add('icon--toggled');
     }
   }
 
   hide(event) {
-    // Check if the click is outside the element and if the dropdown is toggled
-    if (!this.element.contains(event.target) && this.menuTarget.classList.contains('toggled')) {
-      this.menuTarget.classList.remove('toggled');
-
+    if (!this.element.contains(event.target) && this.menuTarget.classList.contains('dropdown__menu--toggled')) {
+      this.menuTarget.classList.remove('dropdown__menu--toggled');
       if (this.checkboxTarget) {
         this.checkboxTarget.checked = false;
       }
+    }
+
+    if(!this.element.contains(event.target) && this.iconTarget.classList.contains('icon--toggled')) {
+      this.iconTarget.classList.remove('icon--toggled');
     }
   }
 }
